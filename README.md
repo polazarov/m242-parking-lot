b5955eab427f3111436d7179b21b1d2adab0dd28
EnesSpahiu <enes.spahiu@noseryoung.ch>
2023-07-11 08:47:47 +0200

    Added project

---
+++ README.md
@@ -0,0 +1,10 @@
+# M5Stack Core2 - LVGL + TOF Distance Sensor Example
+
+The M5Stack Core2 - LVGL + TOF Distance Sensor Example is a project that showcases the capabilities of the M5Stack Core2 development board, the LVGL graphics library, and a Time-of-Flight (TOF) distance sensor. The example demonstrates how to use LVGL to create an interactive user interface and how to integrate the TOF distance sensor to measure the distance between the sensor and an object. 
+
+# Links
+ - ToF Sensor Library: https://registry.platformio.org/libraries/pololu/VL53L0X/installation
+ - Tof Sensor Example: https://github.com/pololu/vl53l0x-arduino/blob/master/examples/Continuous/Continuous.ino
+ - Used LVGL Example: https://github.com/mhaberler/m5core2-lvgl-demo
+ - LVGL 7.x Docs: https://docs.lvgl.io/7.11/
+ - Original chinese source: https://macsbug.wordpress.com/2021/06/18/how-to-run-lvgl-on-m5stack-esp32/
---
+++ src/main.cpp
@@ -0,0 +1,128 @@
+//=====================================================================
+// LVGL : How-to
+//      : M5Core2 slow fps. Scrol slider breaks when moving horizontally
+// 2 Dec,2020
+// https://forum.lvgl.io
+//  /t/m5core2-slow-fps-scrol-slider-breaks-when-moving-horizontally/3931
+// Arduino IDE 1.8.15
+// https://github.com/mhaberler/m5core2-lvgl-demo
+// Check : 2021.06.13 : macsbug
+// https://macsbug.wordpress.com/2021/06/18/how-to-run-lvgl-on-m5stack-esp32/
+//=====================================================================
+
+#include <Arduino.h>
+#include "view.h"
+#include "networking.h"
+#include "sideled.h"
+#include <Wire.h>
+#include <SPI.h>
+#include <VL53L0X.h>
+#include <thread>
+#include <iostream>
+#include <chrono>
+
+lv_obj_t * labelDesc;
+lv_obj_t * labelValue;
+
+VL53L0X sensor;
+
+void init_gui_elements() {
+  labelDesc = add_label("Distanz: ", 20, 20);
+  labelValue = add_label("...", 100, 20);
+}
+
+void timerCallback()
+{
+    mqtt_publish("parking_garage", "Parking is used too long");
+    //Telegram Stuff
+}
+
+void startTimer(int duration)
+{
+    // Sleep for the specified duration
+    std::this_thread::sleep_for(std::chrono::seconds(duration));
+
+    // Invoke the callback function
+    timerCallback();
+}
+
+void init_sensor() {
+  Wire.begin();
+  sensor.setTimeout(500);
+  if (!sensor.init())
+  {
+    Serial.println("Failed to detect and initialize sensor!");
+    while (1) {}
+  }
+  sensor.startContinuous();
+}
+
+void mqtt_callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Testing\n");
+
+  // Parse Payload into Colour string
+  char *buf = (char *)malloc((sizeof(char) * (length + 1)));
+  memcpy(buf, payload, length);
+  buf[length] = '\0';
+  String payloadS = String(buf);
+  payloadS.trim();
+  Serial.printf("%s = %s\n", String(topic), payloadS);
+
+  if (String(topic) == "parking_garage")
+  {
+    if (payloadS == "Free")
+    {
+      lv_label_set_text(labelValue, "2 Open Parkings");
+    } else if (payloadS == "Used") {
+      lv_label_set_text(labelValue, "0 Open Parkings");
+    }
+  }
+}
+
+void setup() {
+  init_m5();
+  init_display();
+  Serial.begin(115200);
+  // Uncomment the following lines to enable WiFi and MQTT
+  lv_obj_t * wifiConnectingBox = show_message_box_no_buttons("Connecting to WiFi...");
+  lv_task_handler();
+  delay(5);
+  setup_wifi();
+  mqtt_init(mqtt_callback);
+  close_message_box(wifiConnectingBox);
+  init_gui_elements();
+  init_sideled();
+  init_sensor();
+
+  set_sideled_state(0,10, SIDELED_STATE_ON);
+}
+
+long nextSensorRead = 0;
+unsigned long next_lv_task = 0;
+//=====================================================================
+void loop(){
+  if(next_lv_task < millis()) {
+    lv_task_handler();
+    next_lv_task = millis() + 5;
+  }
+ if(nextSensorRead < millis()) {
+  unsigned long millimeter_distance = (unsigned long)sensor.readRangeContinuousMillimeters();
+  
+  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+
+  if(millimeter_distance >= 173) {
+    mqtt_publish("parking_garage", "Free");
+    set_sideled_color(0, 10, CRGB::Green);
+  } else {
+    mqtt_publish("parking_garage", "Used");
+    set_sideled_color(0, 10, CRGB::Red);
+  }
+
+  Serial.println();
+  nextSensorRead = millis() + 2000;
+  mqtt_loop();
+ }
+
+}
+//=====================================================================
