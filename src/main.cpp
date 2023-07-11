dd4abe202e1bb36b8d6f7a09ce7d2f8c0d5e33c2
EnesSpahiu <enes.spahiu@noseryoung.ch>
2023-07-11 11:51:33 +0200

    Last changes

--- src/main.cpp
+++ src/main.cpp
@@ -17,42 +17,28 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <VL53L0X.h>
-#include <thread>
 #include <iostream>
 #include <chrono>
 
-lv_obj_t * labelDesc;
-lv_obj_t * labelValue;
+lv_obj_t *labelValue;
 
 VL53L0X sensor;
 
-void init_gui_elements() {
-  labelDesc = add_label("Distanz: ", 20, 20);
-  labelValue = add_label("...", 100, 20);
-}
-
-void timerCallback()
+void init_gui_elements()
 {
-    mqtt_publish("parking_garage", "Parking is used too long");
-    //Telegram Stuff
+  labelValue = add_label(" ", 20, 20);
 }
 
-void startTimer(int duration)
+void init_sensor()
 {
-    // Sleep for the specified duration
-    std::this_thread::sleep_for(std::chrono::seconds(duration));
-
-    // Invoke the callback function
-    timerCallback();
-}
-
-void init_sensor() {
   Wire.begin();
   sensor.setTimeout(500);
   if (!sensor.init())
   {
     Serial.println("Failed to detect and initialize sensor!");
-    while (1) {}
+    while (1)
+    {
+    }
   }
   sensor.startContinuous();
 }
@@ -73,19 +59,22 @@ void mqtt_callback(char *topic, byte *payload, unsigned int length)
   {
     if (payloadS == "Free")
     {
-      lv_label_set_text(labelValue, "2 Open Parkings");
-    } else if (payloadS == "Used") {
-      lv_label_set_text(labelValue, "0 Open Parkings");
+      lv_label_set_text(labelValue, "2 Free Parkingspaces");
+    }
+    else if (payloadS == "Used")
+    {
+      lv_label_set_text(labelValue, "0 Free Parkingspaces");
     }
   }
 }
 
-void setup() {
+void setup()
+{
   init_m5();
   init_display();
   Serial.begin(115200);
   // Uncomment the following lines to enable WiFi and MQTT
-  lv_obj_t * wifiConnectingBox = show_message_box_no_buttons("Connecting to WiFi...");
+  lv_obj_t *wifiConnectingBox = show_message_box_no_buttons("Connecting to WiFi...");
   lv_task_handler();
   delay(5);
   setup_wifi();
@@ -95,34 +84,42 @@ void setup() {
   init_sideled();
   init_sensor();
 
-  set_sideled_state(0,10, SIDELED_STATE_ON);
+  set_sideled_state(0, 10, SIDELED_STATE_ON);
 }
 
 long nextSensorRead = 0;
 unsigned long next_lv_task = 0;
 //=====================================================================
-void loop(){
-  if(next_lv_task < millis()) {
+void loop()
+{
+  if (next_lv_task < millis())
+  {
     lv_task_handler();
     next_lv_task = millis() + 5;
   }
- if(nextSensorRead < millis()) {
-  unsigned long millimeter_distance = (unsigned long)sensor.readRangeContinuousMillimeters();
-  
-  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+  if (nextSensorRead < millis())
+  {
+    unsigned long millimeter_distance = (unsigned long)sensor.readRangeContinuousMillimeters();
 
-  if(millimeter_distance >= 173) {
-    mqtt_publish("parking_garage", "Free");
-    set_sideled_color(0, 10, CRGB::Green);
-  } else {
-    mqtt_publish("parking_garage", "Used");
-    set_sideled_color(0, 10, CRGB::Red);
+    if (sensor.timeoutOccurred())
+    {
+      Serial.print(" TIMEOUT");
+    }
+
+    if (millimeter_distance >= 170)
+    {
+      mqtt_publish("parking_garage", "Free");
+      set_sideled_color(0, 10, CRGB::Green);
+    }
+    else
+    {
+      mqtt_publish("parking_garage", "Used");
+      set_sideled_color(0, 10, CRGB::Red);
+    }
+
+    Serial.println();
+    nextSensorRead = millis() + 2000;
+    mqtt_loop();
   }
-
-  Serial.println();
-  nextSensorRead = millis() + 2000;
-  mqtt_loop();
- }
-
 }
 //=====================================================================
